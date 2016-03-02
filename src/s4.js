a4fe944307464a7111e15b2966c4e7eabce7b890
Tassi Viktor <tassiviktor@users.noreply.github.com>
2016-03-02 22:35:20 +0100

    Added files via upload

--- src/s4.js
+++ src/s4.js
@@ -5,17 +5,18 @@
  *      Denis Ciccale (ki.js)
  *      James Doyle (extend.ki.js) 
  *      Chris Davies (form-to-obj)
- *      ant thanks to http://youmightnotneedjquery.com for some prototypes ;)
+ *      and thanks to http://youmightnotneedjquery.com for some prototypes ;)
  */
-
 !function (b, c, d, e) {
 
     /*
      * init function (internal use)
      * a = selector, dom element or function
      */
-    function i(a, x) {
-        c.push.apply(this, a && a.nodeType ? [a] : '' + a === a ? (typeof x === "object" ? x : (x && b.querySelector(x) ? b.querySelector(x) : b)).querySelectorAll(a) : e)
+    
+
+    function i(a,x) {
+        c.push.apply(this, a && a.nodeType ? [a] : '' + a === a ? (x && typeof x.querySelectorAll == "function")?x:(typeof x === 'string' && /^#/.test(x)?b.getElementById(x.substr(1)):b).querySelectorAll(a) : e)
     }
 
     /*
@@ -24,13 +25,15 @@
      * http://www.dustindiaz.com/smallest-domready-ever
      * returns instance or executes function on ready
      */
-    s4 = function (a, x) {
-        return /^f/.test(typeof a) ? /c/.test(b.readyState) ? a() : $(b).on('DOMContentLoaded', a) : new i(a, x)
+    s4 = function (a,x) {
+        return /^f/.test(typeof a) ? /c/.test(b.readyState) ? a() : $(b).on('DOMContentLoaded', a) : new i(a,x)
     }
     $ = s4;
-    /* set s4 prototype */
+    // set ki prototype
     $[d] = i[d] = $.fn = i.fn = {
+        // default length
         length: 0,
+        splice: c.splice,
         on: function (a, b) {
             return this.each(function (c) {
                 c.addEventListener(a, b)
@@ -41,27 +44,9 @@
                 c.removeEventListener(a, b)
             })
         },
-        /* for testing should be removed later */
-        ready: function (a) {
-            return this.on("DOMContentLoaded", a);
-        },
         each: function (a, b) {
             c.forEach.call(this, a, b)
             return this
-        },
-        is: function (a) {
-            var m = (this[0].matches || this[0].matchesSelector || this[0].msMatchesSelector || this[0].mozMatchesSelector || this[0].webkitMatchesSelector || this[0].oMatchesSelector);
-            if (m) {
-                return m.call(this[0], a);
-            } else {
-                var n = this[0].parentNode.querySelectorAll(a);
-                for (var i = n.length; i--; ) {
-                    if (n[i] === this[0]) {
-                        return true;
-                    }
-                }
-                return false;
-            }
         },
         parent: function () {
             return (this.length < 2) ? $(this[0].parentNode) : [];
@@ -82,82 +67,75 @@
             return this
         },
         css: function (a, b) {
-            if (typeof (a) === 'object') {
+            if ($.type(a) === 'object') {
                 for (var prop in a) {
                     this.each(function (c) {
-                        c.style[prop] = a[prop];
-                    });
+                        c.style[prop] = a[prop]
+                    })
                 }
-                return this;
+                return this
             } else {
                 return b === []._ ? this[0].style[a] : this.each(function (c) {
-                    c.style[a] = b;
-                });
+                    c.style[a] = b
+                })
             }
         },
         hasAttr: function (a) {
-            return this[0].hasAttribute(a);
+            return this[0].hasAttribute(a)
         },
         attr: function (a, b) {
             return b === []._ ? this[0].getAttribute(a) : this.each(function (c) {
-                (a in c) ? c[a] = b : c.setAttribute(a, b);
+                (a in c) ? c[a] = b : c.setAttribute(a, b)
             })
-            return this
         },
         removeAttr: function (a) {
             return this.each(function (b) {
-                b.removeAttribute(a);
+                b.removeAttribute(a)
             });
         },
         data: function (a, b) {
-            return this.attr("data-" + a, b);
+            return this.attr("data-" + a, b)
         },
         aria: function (a, b) {
-            return this.attr("aria-" + a, b);
+            return this.attr("aria-" + a, b)
         },
         hide: function () {
-            this.each(function (b) {
-                b.style.display = 'none';
+            return this.each(function (b) {
+                b.style.display = 'none'
             })
-            return this
         },
         show: function () {
-            this.each(function (b) {
-                b.style.display = '';
+            return this.each(function (b) {
+                b.style.display = ''
             })
-            return this
         },
         before: function (a) {
-            this.each(function (b) {
-                b.insertAdjacentHTML('beforebegin', a);
+            return this.each(function (b) {
+                b.insertAdjacentHTML('beforebegin', a)
             })
-            return this
         },
         append: function (a) {
-            this.each(function (b) {
-                b.innerHTML += a;
+            return this.each(function (b) {
+                b.innerHTML += a
             })
-            return this
         },
         after: function (a) {
-            this.each(function (b) {
-                b.insertAdjacentHTML('afterend', a);
+            return this.each(function (b) {
+                b.insertAdjacentHTML('afterend', a)
             })
-            return this
         },
         remove: function () {
             return this.each(function (b) {
-                b.parentNode.removeChild(b);
+                b.parentNode.removeChild(b)
             });
         },
         replaceWith: function (a) {
-            this.each(function (b) {
-                b.outerHTML = a;
+            return this.each(function (b) {
+                b.outerHTML = a
             })
-            return this
         },
         first: function () {
-            return this[0];
+            return this[0]
         },
         last: function () {
             return this[this.length - 1];
@@ -167,25 +145,24 @@
         },
         html: function (a) {
             return a === []._ ? this[0].innerHTML : this.each(function (c) {
-                c.innerHTML = a;
+                c.innerHTML = a
             })
-            return this
         },
         text: function (a) {
             return a === []._ ? this[0].textContent : this.each(function (b) {
-                b.textContent = a;
+                b.textContent = a
             });
         },
         val: function (a) {
             var r = [];
             a === []._ ?
                     (this[0].options && this[0].getAttribute("multiple") ?
-                            s4.each(this[0], function (x, y) {
+                            $.each(this[0], function (x, y) {
                                 (y.selected) ? r.push(y.value) : 0;
                             }) : r.push(this[0].value))
                     : this.each(function (b) {
                         b.options && b.getAttribute("multiple") ?
-                                s4.each(b, function (x, y) {
+                                $.each(b, function (x, y) {
                                     (a.indexOf(y.value) > -1) ? y.selected = true : y.selected = false;
                                 })
                                 :
@@ -193,55 +170,97 @@
                     });
             return r;
         },
-        serializeJSON: function () {
+        serializeArray: function () {
+            var q = [];
+            this.each(function (c, d) {
+                $("input, select, checkbox, radio, textarea, div[contenteditable='true']", c).each(function (x) {
+                    if (!x.disabled && x.type !== 'file') {
+                        if (x.name) {
+                            if (x.nodeName == "INPUT" && ['checkbox', 'radio'].indexOf(x.type) > -1 && x.checked) {
+                                q.push({name: x.name, value: x.value});
+                            } else if (x.nodeName == "SELECT" && x.type == 'select-multiple') {
+                                for (var j = x.options.length - 1; j >= 0; j--)
+                                    if (x.options[j].selected)
+                                        q.push({name: x.name, value: x.options[j].value});
+                            } else {
+                                q.push({name: x.name, value: x.value});
+                            }
+                        } else if (x.nodeName == 'DIV' && x.getAttribute('contenteditable')) {
+                            q.push({name: x.getAttribute('data-name'), value: x.innerHTML});
+                        }
+                    }
+                })
+            })
+            return q;
         },
         serialize: function () {
+            return bP(this.serializeArray());
+        },
+        serializeJSON: function () {
+            return JSON.stringify(this.serializeArray());
+        },
+        load: function (a, b, c) {
 
         },
-        splice: c.splice
-    };
-    s4.now = function () {
+    }
+    eUC = encodeURIComponent;
+    /*Buildparams*/
+    function bP(obj, pr) {
+        var str = [];
+        if ($.isArray(obj)) {
+            $.each(obj, function (o, a) {
+                var k = pr ? pr + "[" + a['name'] + "]" : a['name'], v = a['value'];
+                str.push($.type(v) == "object" ? $.bP(v, k) : eUC(k) + "=" + eUC(v));
+            });
+        } else if ($.type(obj) == "object") {
+            for (var p in obj) {
+                var k = pr ? pr + "[" + p + "]" : p, v = obj[p];
+                str.push($.type(v) == "object" ? $.bP(v, k) : eUC(k) + "=" + eUC(v));
+            }
+        } else {
+            return obj;
+        }
+        return str.join("&");
+    }
+    $.now = function () {
         return Date.now();
     }
-    s4.type = function (a) {
-        return Object.prototype.toString.call(a).replace(/^\[object (.+)\]$/, '$1').toLowerCase();
+    // typeof null returns “object”
+    $.type = function (a) {
+        return ({}).toString.call(a).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
     }
-    s4.inArray = function (a, b) {
+    $.inArray = function (a, b) {
         return b.indexOf(a)
     }
-    s4.isArray = function (a) {
-        Array.isArray(a)
+    $.isArray = function (a) {
+        return Array.isArray(a)
     }
-    s4.map = function (a, b) {
-        a.map(b) /*Untested*/
+    $.map = function (a, b) {
+        return a.map(b) /*Untested*/
     }
-    s4.trim = function (a) {
+    $.trim = function (a) {
         return a.trim();
     }
-    s4.parseJSON = function (a) {
+    $.parseJSON = function (a) {
         return JSON.parse(a)
     }
-    s4.parseHTML = function (a) {
+    $.parseHTML = function (a) {
         var b = document.implementation.createHTMLDocument();
         b.body.innerHTML = a;
         return b.body.children
     }
 
-    s4.each = function (arr, callback) {
+    $.each = function (arr, callback) {
         for (var i = 0, l = arr.length; i < l; ++i) {
             callback.call(arr[i], i, arr[i]);
         }
         return this;
     }
-    s4.param = function (obj, prefix) {
-        var str = [];
-        for (var p in obj) {
-            var k = prefix ? prefix + "[" + p + "]" : p, v = obj[p];
-            str.push(typeof v == "object" ? $.param(v, k) : encodeURIComponent(k) + "=" + encodeURIComponent(v));
-        }
-        return str.join("&");
+
+    $.param = function (o) {
+        return bP(o);
     }
-    s4.stop = function (e) {
+    $.stop = function (e) {
         if (!e.preventDefault) {
             e.returnValue = false;
         } else {
@@ -249,9 +268,91 @@
         }
     }
 
-    s4.ajax = function (a) {
-    }
+    $.ajax = function (a) {
+        var ct="Content-Type"
+        var f="function"
+        function e(r, s, rt) {
+            if ($.type(a.error) == f)
+                a.error(r, s, rt);
+            c(r, s);
+        }
+        function c(r, s) {
+            if ($.type(a.complete) == f)
+                a.complete(r, s);
+        }
+        function s(rt, s, r) {
+            if ((a.dataType && a.dataType == "json") || (!a.dataType && r.getResponseHeader('Content-Type').indexOf("/json") > -1)) {
+                try {
+                    x = JSON.parse(rt);
+                    rt = x;
+                } catch (exception) {
+                    e(wr, "parsererror", rt);
+                    return;
+                }
+            }
+            if ($.type(a.success) == f) {
+                a.success(rt, s, r);
+            }
+            c(r, s);
+        }
+        if (!a.data)
+            a.data = null;
+        if (!a.headers)
+            a.headers = {};
 
-    s4.getJSON = function (a, b) {
+        var d = a.data;
+        var g = (a.type && ["GET", "POST"].indexOf(a.type) > -1) ? a.type : 'GET';
+        var r = new XMLHttpRequest();
+
+        dct = 'application/x-www-form-urlencoded; charset=UTF-8';
+
+        (!a.headers[ct]) && g !== "GET" ? a.headers[ct] = ((a.contentType) ? a.contentType : dct) : null;
+
+        if (!a.processData || a.processData !== false) {
+            if (!a.headers[ct] || a.headers[ct].indexOf("x-www-form-urlencoded") > -1) {
+                d = $.param(a.data);
+            }
+        }
+
+        r.open(g, a.url + (g == "GET" && d ? "?" + d : ""), true, a.username, a.password);
+        r.onload = function () {
+            if (a.statusCode && $.type(a.statusCode[r.status]) == f)
+                a.statusCode[r.status]();
+
+            (r.status >= 200 && r.status < 400) ?
+                    s(r.responseText, r.status == 304 ? "notmodified" : "success", r) :
+                    e(r, "error", r.responseText);
+
+        }
+
+        if (a.timeout && a.timeout > 0) {
+            setTimeout(function () {
+                if (r.readyState < 4) {
+                    r.abort();
+                    e(r, "timeout");
+                }
+            }, a.timeout);
+        }
+
+        r.onerror = function () {
+            e(r, "error", r.responseText);
+        }
+
+        a.headers["X-Requested-With"] = 'XMLHttpRequest';
+        if ($.type(a.beforeSend) == f)
+            a.beforeSend(r, a);
+        Object.keys(a.headers).forEach(function (k) {
+            r.setRequestHeader(k, a.headers[k]);
+        });
+
+        (g == "GET") ? r.send() : r.send(d);
+
+        return r;
+    }
+    $.get = function (a, b, s, d) {
+        $.ajax({url: a, data: b, success: s, dataType: d});
+    }
+    $.post = function (a, b, s, d) {
+        $.ajax({type: "POST", url: a, data: b, success: s, dataType: d});
     }
 }(document, [], 'prototype');
